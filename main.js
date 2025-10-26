6e61076961337e4726b962a9432227c1ed6f1c21
Austin <20157814+adurr21@users.noreply.github.com>
2025-10-26 11:10:01 -0700

    Added ThreeJS model Added threejs rotating city model.

--- main.js
+++ main.js
@@ -1,1 +1,50 @@
-import * as THREE from 'three';
+import * as three from 'three';
+import { GLTFLoader, OrbitControls } from 'three/examples/jsm/Addons.js';
+import cityModel from './assets/model/Little City.glb';
+
+// ThreeJS
+
+const scene = new three.Scene();
+const camera = new three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
+
+const renderer = new three.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+
+// City
+const loader = new GLTFLoader();
+loader.load(cityModel, (gltf) => {
+    const model = gltf.scene;
+
+    model.scale.setScalar(2);
+
+    model.rotateY(Math.PI)
+
+    scene.add(model);
+
+}, undefined, (error) => {
+    console.error(error);
+});
+
+const directionalLight = new three.DirectionalLight(0xffffff, 3);
+scene.add(directionalLight);
+
+const ambientLight = new three.AmbientLight(0xffffff, 1);
+scene.add(ambientLight);
+
+// Camera Controls
+const controls = new OrbitControls(camera, renderer.domElement);
+camera.position.set(0, 2, 5);
+controls.target.set(0.5, -0.5, 0);
+controls.autoRotate = true;
+controls.autoRotateSpeed = 0.75;
+
+directionalLight.position.set(0, 2, 5)
+
+scene.background = new three.Color(0xffffff);
+
+renderer.setAnimationLoop(() => {
+    controls.update();
+    renderer.render(scene, camera);
+})
